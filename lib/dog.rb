4095f233fd296fe5551a7847a04a556ec7d563d2
kkirby16 <kkirby16@my.whitworth.edu>
2019-12-11 20:29:17 -0800

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,29 @@
+class Dog
+    # def initialize
+    #     @name= name
+    # end
+
+    def name=(name) 
+        @name = name 
+    end
+     
+    def name
+        @name
+    end
+
+   def breed=(breed) #always will be set in this orientation. #attr reader is the getter and the attr writer is the setter 
+        @breed = breed
+   end 
+
+   def breed
+      @breed  
+   end 
+end 
+
+
+
+
+
+
+
+
---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person 
+
+    def name=(name) #setter 
+        @name = name 
+    end 
+
+    def name  #getter 
+        @name 
+    end 
+
+    def job=(job) #setter 
+        @job = job 
+    end 
+
+    def job  #getter 
+        @job
+    end 
+end 
