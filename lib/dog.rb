98670d988b9819182173d3600eac41d06dc028ae
github username <github email address>
2020-01-17 14:53:09 -0800

    Redone

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,18 +1,3 @@
 class Dog
-    
-    def name=(new_name)
-        @name = new_name
-    end
-
-    def name
-        @name
-    end
-
-    def breed=(new_breed)
-        @breed = new_breed
-    end
-
-    def breed
-        @breed
-    end
+  attr_accessor :name, :breed
 end
